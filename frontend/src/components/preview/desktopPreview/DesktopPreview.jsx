9571961d27c06e0c9f69d2a2531dd4441e6f9402
CASV331 <arturosandoval688@gmail.com>
2025-12-15 19:22:17 -0700

    image added

--- frontend/src/components/preview/desktopPreview/DesktopPreview.jsx
+++ frontend/src/components/preview/desktopPreview/DesktopPreview.jsx
@@ -1,9 +1,17 @@
 function Preview() {
   return (
-    <div className="w-full max-w-4xl max-h-96 p-2 border-4 rounded-lg border-gray-700 desktop-preview-container">
-      <div className="w-full h-full p-4 bg-gray-700 border-[3px] border-[#6ae1ff] rounded-lg desktop-preview">
-      <h2 className="desktop-preview__title">Desktop Preview</h2>
-      {/* Add more preview elements here */}
+    <div className="w-full max-w-4xl max-h-96 p-2 border-4 rounded-lg border-gray-700">
+      <div
+      className="w-full h-full border-[3px] border-[#6ae1ff] rounded-lg desktop-preview"
+      style={{
+        backgroundImage: "url('/AsaDen.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat"
+      }}>
+      <div className="w-full h-full rounded-lg bg-amber-200">
+
+      </div>
     </div>
     </div>
   );
