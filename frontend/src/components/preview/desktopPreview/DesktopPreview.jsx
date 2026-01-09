8659790f73916ee938e1fa95103ffb2cfb7fae6f
CASV331 <arturosandoval688@gmail.com>
2026-01-08 17:06:28 -0700

    little changes

--- frontend/src/components/preview/desktopPreview/DesktopPreview.jsx
+++ frontend/src/components/preview/desktopPreview/DesktopPreview.jsx
@@ -2,15 +2,19 @@ function Preview() {
   return (
     <div className="w-full max-w-4xl max-h-96 p-2 border-4 rounded-lg border-gray-700">
       <div
-      className="w-full h-full border-[3px] border-[#6ae1ff] rounded-lg desktop-preview"
+      className="w-full h-full p-2 rounded-lg desktop-preview"
       style={{
         backgroundImage: "url('/AsaDen.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat"
       }}>
-      <div className="w-full h-full rounded-lg bg-amber-200">
-
+      <div className="w-full h-full rounded-lg border-4 border-amber-400 ">
+      <div className="w-full h-full p-2 bg-gray-800 opacity-80">
+      <div className="text-sm">
+        <strong>[username ~ ]$ </strong>
+      </div>
+      </div>
       </div>
     </div>
     </div>
